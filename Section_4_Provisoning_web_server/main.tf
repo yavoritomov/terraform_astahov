a0d9b69bd95524d45f88cf88daea138c0745bec1
Yavor Tomov <yavoritomov@gmail.com>
2022-09-01 10:49:05 +0300

    upload

---
+++ Section_4_Provisoning_web_server/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-2"
+  profile = "admin"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web_server01" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name  = "Web-Server01"
+    Owner = "Yavor Tomov"
+  }
+}
+
+output "web_server01_tags" {
+  value = ["${aws_instance.web_server01.tags}"]
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
