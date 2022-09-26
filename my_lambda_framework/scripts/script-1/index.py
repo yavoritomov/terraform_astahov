3b12320c273bab062eb5b7927dd44a3c1e80b00b
Yavor Tomov <yavoritomov@gmail.com>
2022-09-26 16:36:03 +0300

    Working on python libs

---
+++ my_lambda_framework/scripts/script-1/index.py
@@ -0,0 +1,10 @@
+import urllib3
+
+def lambda_handler(event, context):
+    http = urllib3.PoolManager()
+    resp = http.request('GET', 'https://api.zippopotam.us/us/77004')
+    response = resp
+    print(response)
+    return {
+       'message' : response
+    }
