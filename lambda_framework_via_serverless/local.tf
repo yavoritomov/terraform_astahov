6fb278d772f4044e1a4da6069342e634cc604b78
Yavor Tomov <yavoritomov@gmail.com>
2022-09-27 09:55:37 +0300

    removing

--- lambda_framework_via_serverless/local.tf
+++
@@ -1,7 +0,0 @@
-locals {
- lambdas = {
-   "script-0" = { memory_size = "128M", schedule = "rate(1 minute)", description="description for 0", description_eventbridge="description for 0" },
-   "script-1" = { memory_size = "128M", schedule = "rate(5 minutes)", description="description for 1", description_eventbridge="description for 1" },
-   "script-2" = { memory_size = "128M", schedule = "rate(1 hour)", description="description for 2", description_eventbridge="description for 2" }
- }
-}
