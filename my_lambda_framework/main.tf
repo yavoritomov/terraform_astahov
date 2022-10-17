35a204745cbb009f5a96970751ecf3576762fb08
yavoritomov <ytomov@hedgeserv.com>
2022-10-17 15:17:57 +0300

    adding sample config for remote state

--- my_lambda_framework/main.tf
+++ my_lambda_framework/main.tf
@@ -1,5 +1,13 @@
+#terraform {
+#  backend "s3" {
+#    bucket         = "<your_bucket_name>"
+#    key            = "terraform.tfstate"
+#    region         = "<your_aws_region>"
+#    dynamodb_table = "<your_dynamo_dbtable_name>"
+#  }
+#}
+
 provider "aws" {
   region = "us-east-2"
- // profile = "admin"
+  profile = "admin"
 }
-
